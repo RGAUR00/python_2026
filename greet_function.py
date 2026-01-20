3b316123646a1fd41980cb94f5408428e0430394
Ansu Sharma <rs5328705@gmail.com>
2026-01-20 13:05:07 +0530

    initial commit for ruchika

---
+++ greet_function.py
@@ -0,0 +1,13 @@
+# def greet():
+#     print("Hello")
+#     print("Welcome to the program!")
+#     print("Have a great day!")
+# greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}!")
+    print(f"Welcome to the program {name}!")
+    print("Have a great day!")
+
+
+greet_with_name(542)
---
+++ weeks_left.py
@@ -0,0 +1,5 @@
+def life_in_weeks(age):
+    x= (90-age)*52
+    print(f"You have {x} weeks left until you turn 90 years old.")
+age=int(input("Enter your age in years:"))
+life_in_weeks(age)
