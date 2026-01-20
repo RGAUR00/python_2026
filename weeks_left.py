9fb277f2dc022fd671ce88fcac5b782c6845088f
RGAUR00 <ruchikagaur2710@gmail.com>
2026-01-20 13:17:59 +0530

    revert back

--- weeks_left.py
+++ weeks_left.py
@@ -3,5 +3,3 @@ def life_in_weeks(age):
     print(f"You have {x} weeks left until you turn 90 years old.")
 age=int(input("Enter your age in years:"))
 life_in_weeks(age)
-
-print("Great workkkkkkk!!!")
